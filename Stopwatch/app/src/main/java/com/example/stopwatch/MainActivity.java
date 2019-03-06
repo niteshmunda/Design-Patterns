043e4ddd98e73760ca911ec84ab4e7117c5f2630
Nitesh Singh Munda <niteshsingh.m@dunzo.in>
2019-03-07 01:13:30 +0530

    Stopwatch

---
+++ Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.stopwatch;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean running;
+    private int second = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        runtimer();
+    }
+
+    private void runtimer() {
+        final TextView textView = findViewById(R.id.timeText);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = second / 3600;
+                int minutes = (second % 3600)/60;
+                int seconds = second%60;
+
+                String time = String.format("%d:%02d:%02d",hours,minutes,seconds);
+                textView.setText(time);
+                if(running)
+                    second++;
+                handler.postDelayed(this,1000);
+            }
+        });
+
+    }
+
+    public void startCLick(View view) {
+        running  = true;
+    }
+
+    public void pauseClick(View view) {
+        running  = false;
+    }
+
+    public void restartCLick(View view) {
+        running = false;
+        second = 0;
+    }
+}
